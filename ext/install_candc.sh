e1114a5708a4fcc3f53779f92c1bbd7d8cfbf909
Valerio Basile <valeriobasile@gmail.com>
2017-10-12 11:18:10 +0200

    added SWI-Prolog 6.6.6 to the C&C installation script

--- ext/install_candc.sh
+++ ext/install_candc.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 cd candc
-ln -s Makefile.unix Makefile
+cat Makefile.unix | sed "s/^PROLOG = swipl/PROLOG = bin\/swipl/" > Makefile
 BASEDIR=`pwd`
 
+# install SOAP
 cd ext
 unzip gsoap_2.8.16.zip
 cd gsoap-2.8
@@ -11,10 +12,23 @@ cd gsoap-2.8
 make
 make install
 
+# install SWI-Prolog 6.6.6
 cd ../../
+wget http://www.swi-prolog.org/download/stable/src/pl-6.6.6.tar.gz
+tar -xzvf pl-6.6.6.tar.gz
+cd pl-6.6.6/
+./configure --prefix=$BASEDIR
 make
-make bin/t
-make bin/boxer
+make install
+cd ../
+
+# compile the C&C tools with SOAP support
+make
 make soap
 
+# compile tokenizer and Boxer
+make bin/t
+make bin/boxer
+
+# unzip the models for the parser
 tar -xjvf models-1.02.tbz2
