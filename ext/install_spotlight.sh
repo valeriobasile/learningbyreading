f4ef45019f3977b6692c5a901b83cb35e6b039df
Valerio Basile <valeriobasile@gmail.com>
2019-09-06 11:08:54 +0200

    fixed typo and URLs

--- ext/install_spotlight.sh
+++ ext/install_spotlight.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 mkdir -p spotlight
-cd spotligh
-wget http://spotlight.sztaki.hu/downloads/dbpedia-spotlight-latest.jar
-wget http://spotlight.sztaki.hu/downloads/latest_models/en.tar.gz
+cd spotlight
+wget https://gitlab.tubit.tu-berlin.de/freecookie/text-processing/blob/master/spotlight-server/dbpedia-spotlight-latest.jar
+wget https://downloads.sourceforge.net/project/dbpedia-spotlight/2016-10/en/model/en.tar.gz
 tar -xzvf en.tar.gz
-
-
-
-# test
-
-curl http://localhost:2222/rest/annotate   --data-urlencode "text=President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance."   --data "confidence=0.35"   -H "Accept: application/json"
-
